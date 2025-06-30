ba095ba4f2d3b5c74096658d0614f5c0db2ab65f
Sameer <sameerrahangdale63@gmail.com>
2025-06-30 14:40:01 +0530

    database connected globally

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json()); 
 
-mongoose.connect("mongodb://127.0.0.1:27017/jobapp")
+mongoose.connect("mongodb+srv://sameerrahangdale63:YymxW8dYcUgXqWKv@cluster0.3knqum2.mongodb.net/")
 .then(() => console.log(`DB Connected successfully`))
 .catch(err => console.log(`Error connecting database`, err));
 
